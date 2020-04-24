3124eb54748e3b2eebcfae43faa8f7c4f40db31e
Chris Mabon <chris.mabon@protonmail.com>
2020-04-24 02:03:35 -0400

    Look and feel tweaks

--- PhasePharmer/static/phase_src/phase.js
+++ PhasePharmer/static/phase_src/phase.js
@@ -48,51 +48,51 @@ let subs = [
 ];
 
 let boxMods = [
-    [0.8, '▪Grow time 20% faster than normal']
-    , [0.6, '▪Grow time 40% faster than normal']
-    , [1.25, '▪Grow time 25% slower per crop']
-    , [1.5, '▪Grow time 50% slower for each crop']
-    , [2.0, '▪Grow time 100% slower than normal']
-    , [1, '▪Chance for Second Mushroom Type: 15% per crop']
-    , [1, '▪Chance for Second Mushroom Type: 33%']
-    , [1, '▪Yield +100% mushrooms per crop']
-    , [1, '▪Yield +200% more mushrooms than normal']
-    , [1, '▪XP +33% more per mushroom']
-    , [1, '▪XP penalty -50% XP per mushroom']
-    , [1, '▪This box requires 2 doses of substrate']
-    , [1, 'No additional effects']
+    [0.80, '++ Grow time 20% faster']             //0
+    , [0.60, '++ Grow time 40% faster']
+    , [1.00, '++ Chance for bonus mushrooms 15%']   //2
+    , [1.00, '++ Chance for bonus mushrooms 33%']
+    , [1.00, '++ Yield +100% more mushrooms']       //4
+    , [1.00, '++ Yield +200% more mushrooms']
+    , [1.00, '++ XP bonus +33%']                    //6
+    , [1.25, '-- Grow time 25% slower']
+    , [1.50, '-- Grow time 50% slower']             //8
+    , [2.00, '-- Grow time 100% slower']
+    , [1.00, '-- XP penalty -50%']                  //10
+    , [1.00, '-- Requires 2 doses of substrate']
+    , [1.00, '▪ No additional box effects']         //12
 ];
 
 let boxes = [
     ['Mushroom Box', [boxMods[12]]]
-    , ['Lucky Box', [boxMods[6]]]
-    , ['Practice Box', [boxMods[2], boxMods[9]]]
-    , ['Epic Crop Box', [boxMods[4], boxMods[8], boxMods[11]]]
+    , ['Lucky Boxes', [boxMods[3]]]
+    , ['Practice Boxes', [boxMods[6], boxMods[7]]]
+    , ['Epic Crop Box', [boxMods[5], boxMods[9], boxMods[11]]]
+    , ['High Yield Box', [boxMods[2], boxMods[4], boxMods[11], boxMods[8]]]
     , ['Fast Box', [boxMods[0], boxMods[10]]]
-    , ['High Yield Box', [boxMods[3], boxMods[5], boxMods[7], boxMods[11]]]
     , ['Very Fast Box', [boxMods[1], boxMods[10]]]
 ];
 
 let mus = [
-    ['Parasol', 2, subs[0], subs[3], '../static/phase_img/parasol.png', [4, 7], [0, 3]]
-    , ['Mycena', 3, subs[1], subs[4], '../static/phase_img/mycena.png', [1, 2], [5, 6]]
-    , ['Boletus', 4, subs[2], subs[5], '../static/phase_img/boletus.png', [0, 5], [1, 4]]
-    , ['Field', 5, subs[0], subs[4], '../static/phase_img/field.png', [3, 6], [2, 7]]
-    , ['Goblin', 5, subs[2], subs[3], '../static/phase_img/goblin.png', [0, 3], [4, 7]]
-    , ['Blusher', 6, subs[2], subs[5], '../static/phase_img/blusher.png', [0, 5], [1, 4]]
-    , ['Milk Cap', 7, subs[0], subs[3], '../static/phase_img/milk.png', [4, 7], [0, 3]]
-    , ['Blastcap', 8, subs[0], subs[5], '../static/phase_img/blastcap.png', [4, 5], [0, 1]]
-    , ['Blood', 8, subs[1], subs[3], '../static/phase_img/blood.png', [1, 6], [2, 3]]
-    , ['Coral', 9, subs[1], subs[5], '../static/phase_img/coral.png', [2, 3], [6, 7]]
-    , ['Iocaine', 10, subs[1], subs[4], '../static/phase_img/iocaine.png', [1, 2], [5, 6]]
-    , ['Groxmax', 11, subs[0], subs[4], '../static/phase_img/groxmax.png', [3, 6], [2, 7]]
-    , ['False Agaric', 12, subs[1], subs[4], '../static/phase_img/false.png', [6, 7], [2, 3]]
-    , ['Porcini', 12, subs[2], subs[5], '../static/phase_img/porcini.png', [4, 5], [0, 1]]
-    , ['Black Foot', 13, subs[2], subs[3], '../static/phase_img/black.png', [0, 7], [6, 6]]
-    , ['Wizard\'s', 13, subs[0], subs[4], '../static/phase_img/wizards.png', [1, 2], [3, 6]]
-    , ['Pixie\'s', 14, subs[0], subs[5], '../static/phase_img/pixies.png', [2, 3], [6, 7]]
-    , ['Fly Amanita', 15, subs[0], subs[4], '../static/phase_img/fly.png', [1, 4], [0, 5]]
-    , ['Charged Mycelium', 16, subs[2], subs[5], '../static/phase_img/charged.png', [0, 3], [7, 7]]
+    ['Parasol', 2, subs[0], subs[3], '/assets/phase_img/parasol.png', [4, 7], [0, 3]]
+    , ['Mycena', 3, subs[1], subs[4], '/assets/phase_img/mycena.png', [1, 2], [5, 6]]
+    , ['Boletus', 4, subs[2], subs[5], '/assets/phase_img/boletus.png', [0, 5], [1, 4]]
+    , ['Field', 5, subs[0], subs[4], '/assets/phase_img/field.png', [3, 6], [2, 7]]
+    , ['Goblin', 5, subs[2], subs[3], '/assets/phase_img/goblin.png', [0, 3], [4, 7]]
+    , ['Blusher', 6, subs[2], subs[5], '/assets/phase_img/blusher.png', [0, 5], [1, 4]]
+    , ['Milk Cap', 7, subs[0], subs[3], '/assets/phase_img/milk.png', [4, 7], [0, 3]]
+    , ['Blastcap', 8, subs[0], subs[5], '/assets/phase_img/blastcap.png', [4, 5], [0, 1]]
+    , ['Blood', 8, subs[1], subs[3], '/assets/phase_img/blood.png', [1, 6], [2, 3]]
+    , ['Coral', 9, subs[1], subs[5], '/assets/phase_img/coral.png', [2, 3], [6, 7]]
+    , ['Iocaine', 10, subs[1], subs[4], '/assets/phase_img/iocaine.png', [1, 2], [5, 6]]
+    , ['Groxmax', 11, subs[0], subs[4], '/assets/phase_img/groxmax.png', [3, 6], [2, 7]]
+    , ['False Agaric', 12, subs[1], subs[4], '/assets/phase_img/false.png', [6, 7], [2, 3]]
+    , ['Porcini', 12, subs[2], subs[5], '/assets/phase_img/porcini.png', [4, 5], [0, 1]]
+    , ['Black Foot', 13, subs[2], subs[3], '/assets/phase_img/black.png', [0, 7], [6, 6]]
+    , ['Wizard\'s', 13, subs[0], subs[4], '/assets/phase_img/wizards.png', [1, 2], [3, 6]]
+    , ['Pixie\'s', 14, subs[0], subs[5], '/assets/phase_img/pixies.png', [2, 3], [6, 7]]
+    , ['Fly Amanita', 15, subs[0], subs[4], '/assets/phase_img/fly.png', [1, 4], [0, 5]]
+    , ['Charged Mycelium', 16, subs[2], subs[5], '/assets/phase_img/charged.png', [0, 3], [7, 7]]
 ];
 
 let robustly = [
@@ -123,8 +123,7 @@ function toggleTimerInfo() {
         table.style.display = 'grid';
         timers.style.display = 'none';
         toggle.innerText = 'Box Timers';
-    }
-    else {
+    } else {
         table.style.display = 'none';
         timers.style.display = 'grid';
         toggle.innerText = 'Phase Info';
@@ -162,29 +161,21 @@ function timeToNextPhase(year, month, day, hour) {
 
     if ((currPhase.valueOf() < 1.0) && (currPhase.valueOf() >= 0.875)) {
         phaseProgress = currPhase.valueOf() - 0.875;
-    }
-    else if ((currPhase.valueOf() < 0.875) && (currPhase.valueOf() >= 0.75)) {
+    } else if ((currPhase.valueOf() < 0.875) && (currPhase.valueOf() >= 0.75)) {
         phaseProgress = currPhase.valueOf() - 0.75;
-    }
-    else if ((currPhase.valueOf() < 0.75) && (currPhase.valueOf() >= 0.625)) {
+    } else if ((currPhase.valueOf() < 0.75) && (currPhase.valueOf() >= 0.625)) {
         phaseProgress = currPhase.valueOf() - 0.625;
-    }
-    else if ((currPhase.valueOf() < 0.625) && (currPhase.valueOf() >= 0.5)) {
+    } else if ((currPhase.valueOf() < 0.625) && (currPhase.valueOf() >= 0.5)) {
         phaseProgress = currPhase.valueOf() - 0.5;
-    }
-    else if ((currPhase.valueOf() < 0.5) && (currPhase.valueOf() >= 0.375)) {
+    } else if ((currPhase.valueOf() < 0.5) && (currPhase.valueOf() >= 0.375)) {
         phaseProgress = currPhase.valueOf() - 0.375;
-    }
-    else if ((currPhase.valueOf() < 0.375) && (currPhase.valueOf() >= 0.25)) {
+    } else if ((currPhase.valueOf() < 0.375) && (currPhase.valueOf() >= 0.25)) {
         phaseProgress = currPhase.valueOf() - 0.25;
-    }
-    else if ((currPhase.valueOf() < 0.25) && (currPhase.valueOf() >= 0.125)) {
+    } else if ((currPhase.valueOf() < 0.25) && (currPhase.valueOf() >= 0.125)) {
         phaseProgress = currPhase.valueOf() - 0.125;
-    }
-    else if (currPhase.valueOf() < 0.125) {
+    } else if (currPhase.valueOf() < 0.125) {
         phaseProgress = currPhase.valueOf()
-    }
-    else {
+    } else {
         phaseProgress = -1.0;
     }
 
@@ -277,8 +268,7 @@ function serverTime() {
 
     if (dstMonth || marchDst || novDst) {
         h = today.getUTCHours() - 4;
-    }
-    else {
+    } else {
         h = today.getUTCHours() - 5;
     }
 
@@ -307,8 +297,7 @@ function nextFull() {
 
     if (kFrac < 0.5) {
         k = 0.5;
-    }
-    else {
+    } else {
         k = 1.5;
     }
 
@@ -335,41 +324,33 @@ function updateInfo() {
     let y = today.getFullYear();
     let tblRobust = document.getElementById('robust_growing');
     let tblDecent = document.getElementById('decent_growing');
-    let tblBoxes = document.getElementById('box_effects');
+    let tblBoxesPos = document.getElementById('box_effects_pos');
+    let tblBoxesNeg = document.getElementById('box_effects_neg');
     let phaser = document.getElementById('phases');
     let boxer = document.getElementById('boxes');
-    let untilNext = document.getElementById('until_next');
-    let row;
-    let icon;
-    let name;
-    let grwTime;
-    let highSubs;
-    let lowSubs;
-    let phasePick;
+    // let untilNext = document.getElementById('until_next');
     let phaseNum = phaser.selectedIndex;
     let boxType = boxer.selectedIndex;
     let rob = 0;
     let dec = 0;
-    let bx = 0;
+    let boxIndxPos = 0;
+    let boxIndxNeg = 0;
     let boxMod = boxes[boxType][1][0][0];
-    let headerR;
-    let bodyR;
-    let bodyD;
-    let bodyB;
 
     tblRobust.innerHTML = '';
     tblDecent.innerHTML = '';
-    tblBoxes.innerHTML = '';
-    untilNext.innerHTML = '';
+    tblBoxesPos.innerHTML = '';
+    tblBoxesNeg.innerHTML = '';
+    // untilNext.innerHTML = '';
 
-    headerR = tblRobust.createTHead();
-    row = headerR.insertRow(rob);
-    icon = row.insertCell(0);
-    name = row.insertCell(1);
-    grwTime = row.insertCell(2);
-    highSubs = row.insertCell(3);
-    lowSubs = row.insertCell(4);
-    phasePick = row.insertCell(5);
+    let headerR = tblRobust.createTHead();
+    let row = headerR.insertRow(rob);
+    let icon = row.insertCell(0);
+    let name = row.insertCell(1);
+    let grwTime = row.insertCell(2);
+    let highSubs = row.insertCell(3);
+    let lowSubs = row.insertCell(4);
+    let phasePick = row.insertCell(5);
 
     icon.appendChild(icon.ownerDocument.createTextNode('Icon'));
     name.appendChild(name.ownerDocument.createTextNode('Mushroom'));
@@ -378,25 +359,34 @@ function updateInfo() {
     lowSubs.appendChild(lowSubs.ownerDocument.createTextNode('Low Substrate'));
     phasePick.appendChild(phasePick.ownerDocument.createTextNode('Phase Ready'));
 
-    bodyR = tblRobust.createTBody();
-    bodyD = tblDecent.createTBody();
-    bodyB = tblBoxes.createTBody();
+    let bodyR = tblRobust.createTBody();
+    let bodyD = tblDecent.createTBody();
+    let bodyBp = tblBoxesPos.createTBody();
+    let bodyBn = tblBoxesNeg.createTBody();
 
-    let timeNext = timeToNextPhase(today.getFullYear(), today.getMonth(), today.getDate(), today.getHours());
-    let sD, sH, sM, sS = 's ';
-    today.getDate().valueOf() > 1 ? sD = 's ' : sD = '';
-    today.getHours().valueOf() > 1 ? sH = 's ' : sH = '';
-    today.getMinutes().valueOf() > 1 ? sM = 's ' : sM = '';
-    today.getSeconds().valueOf() > 1 ? sS = 's ' : sS = '';
-    let txtNode = (timeNext.floorDays + ' day' + sD + timeNext.floorHours + ' hour' + sH +
-        timeNext.floorMins + ' min' + sM + timeNext.floorSecs + ' second' + sS);
-    untilNext.appendChild(untilNext.ownerDocument.createTextNode(txtNode));
+    // let timeNext = timeToNextPhase(today.getFullYear(), today.getMonth(), today.getDate(), today.getHours());
+    // let sD, sH, sM, sS = 's ';
+    // today.getDate().valueOf() > 1 ? sD = 's ' : sD = '';
+    // today.getHours().valueOf() > 1 ? sH = 's ' : sH = '';
+    // today.getMinutes().valueOf() > 1 ? sM = 's ' : sM = '';
+    // today.getSeconds().valueOf() > 1 ? sS = 's ' : sS = '';
+    // let txtNode = (timeNext.floorDays + ' day' + sD + timeNext.floorHours + ' hour' + sH +
+    //     timeNext.floorMins + ' min' + sM + timeNext.floorSecs + ' second' + sS);
+    // untilNext.appendChild(untilNext.ownerDocument.createTextNode(txtNode));
 
-    for (let mod in boxes[boxType][1]) {
-        row = bodyB.insertRow(bx);
-        let cell = row.insertCell(0);
-        cell.appendChild(cell.ownerDocument.createTextNode(boxes[boxType][1][bx][1]));
-        bx += 1;
+    for (let mods in boxes[boxType][1]) {
+        let mod = boxes[boxType][1][mods];
+        if (mod[1].includes('+')) {
+            let rowPos = bodyBp.insertRow(boxIndxPos);
+            let cellPos = rowPos.insertCell(0);
+            cellPos.appendChild(cellPos.ownerDocument.createTextNode(boxes[boxType][1][mods][1]));
+            boxIndxPos += 1;
+        } else if ((mod[1].includes('-')) || (mod[1].includes('▪'))) {
+            let rowNeg = bodyBn.insertRow(boxIndxNeg);
+            let cellNeg = rowNeg.insertCell(0);
+            cellNeg.appendChild(cellNeg.ownerDocument.createTextNode(boxes[boxType][1][mods][1]));
+            boxIndxNeg += 1;
+        }
     }
 
     for (let shroom in mus) {
@@ -405,7 +395,7 @@ function updateInfo() {
         let isDecent = (!isRobust && !isPoor);
 
         if (isRobust) {
-            row = bodyR.insertRow(rob);
+            let row = bodyR.insertRow(rob);
             let mushPic = row.insertCell(0);
             let img = document.createElement('img');
             img.src = mus[shroom][4];
@@ -429,14 +419,13 @@ function updateInfo() {
                 rdyDay += 1;
             }
 
-            console.log(julianDay(y, mo, rdyDay, rdyHour));
+            // console.log(julianDay(y, mo, rdyDay, rdyHour));
 
             let rdyPhase = phaseNameTxt(phaseAt(julianDay(y, mo, rdyDay, rdyHour)));
             phasePick.appendChild(phasePick.ownerDocument.createTextNode(rdyPhase));
             rob += 1;
-        }
-        else if (isDecent) {
-            row = bodyD.insertRow(dec);
+        } else if (isDecent) {
+            let row = bodyD.insertRow(dec);
             let mushPic = row.insertCell(0);
             let img = document.createElement('img');
             img.src = mus[shroom][4];
@@ -493,8 +482,8 @@ function setupApp() {
     let boxNums = document.getElementById('box_nums');
     let boxShrooms = document.getElementById('box_shrooms');
     let boxIndexTxt = document.getElementById('box_index_txt');
-    let currPhaseElem = document.getElementById('curr_phase');
-    let nextPhaseElem = document.getElementById('next_phase');
+    let currPhaseElem = document.getElementById('curr_phase_txt');
+    let nextPhaseElem = document.getElementById('next_phase_txt');
     let toggleButton = document.getElementById('timer_toggle');
 
     // console.log(phaseAt(julianDay(y, mo, d, h)));
