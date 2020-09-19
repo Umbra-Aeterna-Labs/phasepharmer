025753cee5d1c03ad70f908b50d70d3b33a74d26
Chris Mabon <chris.mabon@protonmail.com>
2020-09-19 19:37:43 -0400

    Added license header to relevant files

--- PhasePharmer/assets/phase_src/phase.js
+++ PhasePharmer/assets/phase_src/phase.js
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2020 Umbra Aeterna Labs
+ * This file is part of PhasePharmer <https://github.com/Umbra-Aeterna-Labs/PhasePharmer>.
+ *
+ * PhasePharmer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PhasePharmer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhasePharmer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 let iconSize = 40;
 let maxTimers = 12;
 let hoursPerPhase = 88.59;
