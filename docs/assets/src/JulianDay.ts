455d2b83ba9219ee7e64e2cb883af990e6fbdebf
Chris Mabon <umbra.aeterna.labs@gmail.com>
2025-10-26 13:41:46 -0400

    Add source files with better structuring

---
+++ docs/assets/src/JulianDay.ts
@@ -0,0 +1,14 @@
+/*
+ * PhasePharmer
+ * A mushroom farming web app for Project: Gorgon
+ * Licensed under GPLv3 <https://choosealicense.com/licenses/gpl-3.0>
+ * Maintained by Umbra Aeterna Labs <https://github.com/Umbra-Aeterna-Labs>
+ */
+
+/**
+ * @name JulianDay
+ * @desc " "
+ * " "
+ * " "
+ * " "
+ */
